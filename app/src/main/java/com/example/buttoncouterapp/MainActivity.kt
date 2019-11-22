358b1c31bc08e1c92c7029c720ef206bc797476e
conrad96 <conrad.mugisha@gmail.com>
2019-11-22 14:25:48 +0300

    declared variables

--- app/src/main/java/com/example/buttoncouterapp/MainActivity.kt
+++ app/src/main/java/com/example/buttoncouterapp/MainActivity.kt
@@ -2,8 +2,15 @@ package com.example.buttoncouterapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var editText: EditText? = null
+    private var button: Button? = null
+    private var textView: TextView?= null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
