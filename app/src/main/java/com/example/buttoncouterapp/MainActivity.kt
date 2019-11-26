d31f0b0c5680d8628ede879769ca53a2b198429a
conrad96 <conrad.mugisha@gmail.com>
2019-11-26 08:58:42 +0300

    changed code to display userinput text into textView widget

--- app/src/main/java/com/example/buttoncouterapp/MainActivity.kt
+++ app/src/main/java/com/example/buttoncouterapp/MainActivity.kt
@@ -8,25 +8,23 @@ import android.widget.EditText
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private var editText: EditText? = null
     private var button: Button? = null
     private var textView: TextView?= null
-    private var num = 0
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        editText = findViewById<EditText>(R.id.editText)
+        val editText: EditText = findViewById<EditText>(R.id.editText)
         button = findViewById<Button>(R.id.button)
         textView = findViewById<TextView>(R.id.textView)
 
         textView?.movementMethod = ScrollingMovementMethod()
 
+
         button?.setOnClickListener {
-            num++
-            textView?.append("Clicked $num times \n")
+            var typedText: String = editText.text.toString()
+            textView?.append(" $typedText \n")
         }
     }
 }
