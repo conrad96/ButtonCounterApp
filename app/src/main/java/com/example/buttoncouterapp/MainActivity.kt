e11dc776d3f228022ddb2204b2386b5e1cd2e476
conrad96 <conrad.mugisha@gmail.com>
2019-11-26 09:57:34 +0300

    added logic to landscape ui components

--- app/src/main/java/com/example/buttoncouterapp/MainActivity.kt
+++ app/src/main/java/com/example/buttoncouterapp/MainActivity.kt
@@ -11,22 +11,32 @@ class MainActivity : AppCompatActivity() {
     private var button: Button? = null
     private var textView: TextView?= null
 
+    private var buttonLand: Button?= null
+    private var textViewLand: TextView?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setContentView(R.layout.landscape_layout)
 
         val editText: EditText = findViewById<EditText>(R.id.editText)
+        val editTextLand: EditText = findViewById<EditText>(R.id.editTextLand)
+        textViewLand = findViewById<TextView>(R.id.textViewLand)
+
         button = findViewById<Button>(R.id.button)
         textView = findViewById<TextView>(R.id.textView)
 
         textView?.movementMethod = ScrollingMovementMethod()
 
-
         button?.setOnClickListener {
             var typedText: String = editText.text.toString()
             textView?.append(" $typedText \n")
         }
-
+        //landscape
+        buttonLand = findViewById<Button>(R.id.buttonLand)
+        buttonLand?.setOnClickListener {
+            var typedTextLand: String = editTextLand.text.toString()
+            textViewLand?.append("$typedTextLand \n")
+        }
     }
 }
