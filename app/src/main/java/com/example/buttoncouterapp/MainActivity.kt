9397d114e7d422876b4fb166fc2b061698ce952c
conrad96 <conrad.mugisha@gmail.com>
2019-12-02 01:47:49 +0300

    fixed state issue on screen flip

--- app/src/main/java/com/example/buttoncouterapp/MainActivity.kt
+++ app/src/main/java/com/example/buttoncouterapp/MainActivity.kt
@@ -8,39 +8,32 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+
 private const val TEXT_CONTENTS = "TextContents"
 private var TAG = "LOGCAT: "
 
 class MainActivity : AppCompatActivity() {
-
-
     private var button: Button? = null
     private var textView: TextView?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Log.d(TAG, "onCreate")
         setContentView(R.layout.activity_main)
         val editText: EditText = findViewById<EditText>(R.id.editText)
         button = findViewById<Button>(R.id.button)
         textView = findViewById<TextView>(R.id.textView)
-
         textView?.movementMethod = ScrollingMovementMethod()
-
         button?.setOnClickListener {
             var typedText: String = editText.text.toString()
             textView?.append(" $typedText \n")
-            Log.d(TAG, "onClick invoked")
         }
     }
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        textView?.text = savedInstanceState?.getString(TEXT_CONTENTS, "")
+        textView?.text = savedInstanceState.getString(TEXT_CONTENTS, "")
     }
-
     override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
         super.onSaveInstanceState(outState, outPersistentState)
-        outState?.putString(TEXT_CONTENTS, textView?.text.toString())
+        outState.putString(TEXT_CONTENTS, textView?.text.toString())
     }
-
 }
